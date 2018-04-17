8fd40be19cdceff1a70d3fc554d4f3dd9af59293
John Lewis <lewisjnerd@gmail.com>
2018-04-17 14:45:32 -0400

    Add rayon for parallel computing. Move work to thread.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ extern crate clap;
 extern crate reqwest;
 extern crate serde_json;
 extern crate regex;
+extern crate rayon;
 
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
+use std::thread;
 use serde_json::{Value};
 use regex::{RegexSetBuilder};
+use rayon::prelude::*;
 
 static HACKER_NEWS_URL: &'static str = "https://hacker-news.firebaseio.com/v0/topstories.json";
 
@@ -17,22 +22,31 @@ fn main() {
         .unwrap();
     let client = reqwest::Client::new();
 
-    let raw_text = client.get(HACKER_NEWS_URL).send().expect("Unable to get topstories");
+    let (tx, rx): (Sender<Vec<Value>>, Receiver<Vec<Value>>) = mpsc::channel();
 
-    let json: Value = serde_json::from_reader(raw_text).expect("unable to parse topstories");
+    thread::spawn(move || {
+        let raw_text = client.get(HACKER_NEWS_URL).send().expect("Unable to get topstories");
 
-    let thing = json.as_array().unwrap();
+        let json: Value = serde_json::from_reader(raw_text).expect("unable to parse topstories");
+
+        let thing = json.as_array().unwrap();
     
-    let top_stories = thing.into_iter().
-        take(100).
-        map(|item| {
-            let story_url = format!("https://hacker-news.firebaseio.com/v0/item/{}.json", &item);
-            serde_json::from_reader(client.get(&story_url).send().unwrap()).unwrap()
-        }).
-        filter(|story: &Value|{
-            set.is_match(story["title"].as_str().unwrap_or(""))
-        })
-        .collect::<Vec<Value>>();
+        let top_stories = thing.par_iter().
+            take(300).
+            map(|item| {
+                let story_url= format!("https://hacker-news.firebaseio.com/v0/item/{}.json", &item);
+                serde_json::from_reader(client.get(&story_url).send().unwrap()).unwrap()
+            }).
+            filter(|story: &Value|{
+                set.is_match(story["title"].as_str().unwrap_or(""))
+            })
+            .collect::<Vec<Value>>();
+
+            tx.send(top_stories).unwrap();
+    });
+    
+    println!("Fetching Stories....");
+    let top_stories = rx.recv().unwrap();
 
     for story in top_stories { 
         println!("story title: {}, comments: {}", story["title"], story["kids"].as_array().unwrap_or(&Vec::new()).len());
